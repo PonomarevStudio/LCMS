dcf66ca9cf19e1dbd47b2992062a48f673d661c6
PonomareVlad <Git@PonomareVlad.ru>
2022-11-19 05:42:16 +0500

    Env-based importmap

--- src/template.mjs
+++ src/template.mjs
@@ -3,7 +3,20 @@ import {readFileSync} from "fs"
 import '../components/lit-router.mjs'
 
 const dev = process.env.VERCEL_ENV === 'development',
-    packageData = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)).toString())
+    packageData = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)).toString()),
+    devImports = {
+        "svalit/": "/packages/svalit/",
+        "@svalit/router": "/packages/router/index.mjs",
+        "@svalit/components/": "/packages/components/",
+        "@svalit/simple-counter": "/packages/simple-counter/index.mjs"
+    },
+    imports = {
+        "#root/": "/",
+        "#lib/": "/lib/",
+        "#utils": "/lib/utils.mjs",
+        "#db": "/lib/db/client.mjs",
+        ...(dev ? devImports : {})
+    }
 
 export const options = {
     dev,
@@ -12,18 +25,7 @@ export const options = {
         ignore: ['mongodb', 'fs/promises'],
         resolutions: packageData?.overrides,
         rootUrl: new URL('../', import.meta.url),
-        inputMap: {
-            imports: {
-                "#root/": "/",
-                "#lib/": "/lib/",
-                "#utils": "/lib/utils.mjs",
-                "#db": "/lib/db/client.mjs",
-                "svalit/": "/packages/svalit/",
-                "@svalit/router": "/packages/router/index.mjs",
-                "@svalit/components/": "/packages/components/",
-                "@svalit/simple-counter": "/packages/simple-counter/index.mjs"
-            }
-        }
+        inputMap: {imports}
     },
     shim: {shimMode: true, mapOverrides: true},
     imports: ['/components/lit-router.mjs'],
